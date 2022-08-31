109cf6d6fb84186b592e41c10ceb3d29b8f01a2a
Naser Sami <ponanzar@gmail.com>
2022-08-31 20:29:04 +0300

    create login model

--- lib/domain/models.dart
+++ lib/domain/models.dart
@@ -1,5 +1,9 @@
 
+
+// **************************************************************************
 // ! onboarding models
+// **************************************************************************
+
 class SliderObject {
   String title, subtitle, image;
   SliderObject(this.title, this.subtitle, this.image);
@@ -10,4 +14,28 @@ class SliderViewObject {
   int numberOfSliders;
   int currentIndex;
   SliderViewObject(this.sliderObject, this.numberOfSliders, this.currentIndex);
+}
+
+// **************************************************************************
+// ! login models
+// **************************************************************************
+
+class Customer {
+  String id;
+  String name;
+  int numOfNotifications;
+  Customer(this.id, this.name, this.numOfNotifications);
+}
+
+class Contacts {
+  String phone;
+  String email;
+  String link;
+  Contacts(this.phone, this.email, this.link);
+}
+
+class Authentication {
+  Customer customer;
+  Contacts contacts;
+  Authentication(this.customer, this.contacts);
 }
