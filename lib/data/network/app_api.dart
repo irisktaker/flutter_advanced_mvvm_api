733640dc0f137c69ae564f2faf30f9be8a331870
Naser Sami <ponanzar@gmail.com>
2022-08-31 20:17:43 +0300

    add_api_service_client(Retrofit)

---
+++ lib/data/network/app_api.dart
@@ -0,0 +1,18 @@
+
+import 'package:advanced_flutter/app/constants.dart';
+import 'package:advanced_flutter/data/response/responses.dart';
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+factory AppServiceClient(Dio dio,{String baseUrl}) = _AppServiceClient;
+
+@POST('/customers/login')
+Future<AuthenticationResponse> login(
+  @Field('email') email, 
+  @Field('password') password
+  );
+}
