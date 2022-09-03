8b5ce03fd2a89d01fd8742ef47f688f98f43a3d2
Naser Sami <ponanzar@gmail.com>
2022-09-03 20:18:51 +0300

    Data Layer - Adding Responses Messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -18,13 +18,32 @@ enum DataSource {
 
 class ResponseCode {
   
-  static const String SUCCESS = "Success."; //200. success, with data
-  static const String NO_CONTENT = "Success."; //201. success, with no data (no content)
-  static const String BAD_REQUEST = "Bad request, Try again later."; // 400. failure, API rejected request
-  static const String FORBIDDEN = "Forbidden request, Try again later."; //403. failure, API rejected request
-  static const String UNAUTHORIZED = "User is unauthorized, Try again later."; //401. failure, user is not authorized
-  static const String NOT_FOUND = "Page not found."; //404. failure, page not found
-  static const String INTERNET_SERVER_ERROR = "Some thing went wrong, Try again later."; //500. failure, crash in server side
+  static const int SUCCESS = 200;                 //success, with data
+  static const int NO_CONTENT = 201;              //success, with no data (no content)
+  static const int BAD_REQUEST = 400;             //failure, API rejected request
+  static const int FORBIDDEN = 403;               //failure, API rejected request
+  static const int UNAUTHORIZED = 401;            //failure, user is not authorized
+  static const int NOT_FOUND = 404;               //failure, page not found
+  static const int INTERNET_SERVER_ERROR = 500;   //failure, crash in server side
+
+  //? local status code
+  static const int CONNECTION_TIMEOUT = -1;
+  static const int CANCEL = -2;
+  static const int RECEIVE_TIMEOUT = -3;
+  static const int SEND_TIMEOUT = -4;
+  static const int CACHE_ERROR = -5;
+  static const int NO_INTERNET_CONNECTION = -6;
+  static const int UNKNOWN = -7;
+}
+
+class ResponseMessage {
+  static const String SUCCESS = "Success.";
+  static const String NO_CONTENT = "Success.";
+  static const String BAD_REQUEST = "Bad request, Try again later."; 
+  static const String FORBIDDEN = "Forbidden request, Try again later.";
+  static const String UNAUTHORIZED = "User is unauthorized, Try again later."; 
+  static const String NOT_FOUND = "Page not found."; 
+  static const String INTERNET_SERVER_ERROR = "Some thing went wrong, Try again later.";
 
   //? local status code
   static const String CONNECTION_TIMEOUT = "Time out error, Try again later";
