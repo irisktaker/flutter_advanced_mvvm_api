2e2bf0424517153a2d7ac6e3f78e6f94bce15d80
Naser Sami <ponanzar@gmail.com>
2022-09-05 20:50:29 +0300

    Binding Login ViewModel to Login View

--- lib/presentation/login/view/login_view.dart
+++ lib/presentation/login/view/login_view.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_flutter/presentation/login/viewmodel/login_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,6 +9,28 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  final LoginViewModel _viewModel = LoginViewModel(_loginUseCase);
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start(); // tell the viewModel to start it's job
+    _usernameController.addListener(() => _viewModel.setUsername(_usernameController.text));
+    _passwordController.addListener(() => _viewModel.setPassword(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
