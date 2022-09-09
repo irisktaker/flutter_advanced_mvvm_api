a97ea37e0e9bea33e33b62b06dfa9421aa2bb9af
Naser Sami <ponanzar@gmail.com>
2022-09-09 09:16:42 +0300

    Add dependency injection Instances

--- lib/app/di.dart
+++ lib/app/di.dart
@@ -1,5 +1,13 @@
+import 'package:advanced_flutter/data/data_source/remote_data_source.dart';
+import 'package:advanced_flutter/data/network/app_api.dart';
+import 'package:advanced_flutter/data/network/dio_factory.dart';
+import 'package:advanced_flutter/data/repository/repository_impl.dart';
+import 'package:advanced_flutter/domain/repository/repository.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:advanced_flutter/data/network/network_info.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 import 'package:advanced_flutter/app/app_prefs.dart';
 
@@ -13,8 +21,23 @@ Future<void> initAppModule() async
   instance.registerLazySingleton<SharedPreferences>(() => sharedPref);
 
   // app preference instance
-  instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance<SharedPreferences>()));
+  instance.registerLazySingleton<AppPreferences>(() => AppPreferences(instance<SharedPreferences>())); // it can be like -> instance() 
 
+  // network info
+  instance.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(InternetConnectionChecker()));
+
+  // dio factory
+  instance.registerLazySingleton<DioFactory>(() => DioFactory(instance<AppPreferences>())); //or just instance()
+  Dio dio = await instance<DioFactory>().getDio();
+
+  // app service client
+  instance.registerLazySingleton<AppServiceClient>(() => AppServiceClient(dio));
+
+  // remote data source
+  instance.registerLazySingleton<RemoteDataSource>(() => RemoteDataSourceImpl(instance<AppServiceClient>()));
+
+  // repository
+  instance.registerLazySingleton<Repository>(() => RepositoryImpl(instance<RemoteDataSource>(), instance<NetworkInfo>()));
 }
 
 Future<void> initLoginModule() async {}
