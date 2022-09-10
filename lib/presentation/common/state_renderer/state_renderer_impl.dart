3cb18733ea4b554d7d11d214e0cf4257266d22b7
Naser Sami <ponanzar@gmail.com>
2022-09-10 12:04:59 +0300

    add implementaion for (error, empty, content states) cases

--- lib/presentation/common/state_renderer/state_renderer_impl.dart
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -2,6 +2,8 @@
 import 'package:advanced_flutter/app/constants.dart';
 import 'package:advanced_flutter/presentation/common/state_renderer/state_renderer.dart';
 import 'package:advanced_flutter/presentation/resources/all_resources.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 abstract class FlowState {
   StateRendererType getStateRendererType();
@@ -57,3 +59,84 @@ class EmptyState extends FlowState {
   @override 
   StateRendererType getStateRendererType() => StateRendererType.FULL_SCREEN_EMPTY_STATE;
 }
+
+extension FlowStateExtension on FlowState {
+  Widget getScreenWidget(BuildContext context, Widget contentScreenWidget, Function retryActionFunction) {
+  
+  // means which one of the flow states is on runtime (runtimeType) now 
+  // this means all the flow states
+    switch (runtimeType) {
+      case LoadingState: // popup or full screen
+      { 
+        if(getStateRendererType() == StateRendererType.POPUP_LOADING_STATE) 
+        {
+          // show loading popup
+          showPopUp(context, getStateRendererType(), getMessage());
+
+          // show content behind popup
+          return contentScreenWidget;
+
+        } 
+        
+        else 
+        {
+          // full screen loading state
+          return StateRenderer(
+            stateRendererType: getStateRendererType(),
+            message: getMessage(),
+            retryActionFunction: retryActionFunction,
+          );
+        }
+      }
+
+      case ErrorState: {
+        if(getStateRendererType() == StateRendererType.POPUP_ERROR_STATE) 
+        {
+          // show error popup 
+          showPopUp(context, getStateRendererType(), getMessage()); // getStateRendererType() == StateRendererType.POPUP_ERROR_STATE
+
+          // show content behind popup
+          return contentScreenWidget;
+        } 
+
+        else 
+        {
+          // full screen
+          return StateRenderer(
+            stateRendererType: getStateRendererType(),
+            message: getMessage(),
+            retryActionFunction: retryActionFunction,
+          );
+        }
+      }
+
+      case ContentState: {
+        return contentScreenWidget;
+      }
+
+      case EmptyState: {
+        return StateRenderer(
+          stateRendererType: getStateRendererType(),
+          message: getMessage(),
+          retryActionFunction: (){}, // empty 
+        );
+      }
+        
+      default: {
+        return contentScreenWidget;
+      }
+    }
+  }
+
+  showPopUp(BuildContext context, StateRendererType stateRendererType, String message) {
+    WidgetsBinding.instance.addPostFrameCallback((_) => showDialog(
+      context: context, 
+      builder: (BuildContext context){
+        return StateRenderer(
+          stateRendererType: stateRendererType,
+          message: message,
+          retryActionFunction: (){},
+        );
+    }));
+  }
+} 
