58d956f23fb60797ac7f89094e3ad5b843fb50db
Naser Sami <ponanzar@gmail.com>
2022-09-10 10:39:54 +0300

    Applying State Renderer - Error State - Empty State - Content State

--- lib/presentation/common/state_renderer/state_renderer_impl.dart
+++ lib/presentation/common/state_renderer/state_renderer_impl.dart
@@ -1,4 +1,5 @@
 
+import 'package:advanced_flutter/app/constants.dart';
 import 'package:advanced_flutter/presentation/common/state_renderer/state_renderer.dart';
 import 'package:advanced_flutter/presentation/resources/all_resources.dart';
 
@@ -7,12 +8,11 @@ abstract class FlowState {
   String getMessage();
 }
 
-//! Loading State (PopUp or Full Screen)
-
+//? Loading State (PopUp or Full Screen)
 class LoadingState extends FlowState {
   StateRendererType stateRendererType;
   String message;
-  LoadingState({required this.stateRendererType, this.message = StringManager.loading});
+  LoadingState(this.stateRendererType, [this.message = StringManager.loading]);
 
   @override
   String getMessage() => message;
@@ -20,4 +20,40 @@ class LoadingState extends FlowState {
   @override
   StateRendererType getStateRendererType() => stateRendererType;
 
+}
+
+//! Error State (PopUp or Full Screen)
+class ErrorState extends FlowState {
+  StateRendererType stateRendererType;
+  String message;
+  ErrorState(this.stateRendererType, this.message);
+
+  @override 
+  String getMessage() => message;
+
+  @override 
+  StateRendererType getStateRendererType() => stateRendererType;
+}
+
+// Content State 
+class ContentState extends FlowState {
+  ContentState();
+
+  @override 
+  String getMessage() => Constants.empty;
+
+  @override 
+  StateRendererType getStateRendererType() => StateRendererType.CONTENT_STATE;
+}
+
+//! Empty State (Full Screen)
+class EmptyState extends FlowState {
+  String message;
+  EmptyState(this.message);
+
+  @override 
+  String getMessage() => message;
+
+  @override 
+  StateRendererType getStateRendererType() => StateRendererType.FULL_SCREEN_EMPTY_STATE;
 }
