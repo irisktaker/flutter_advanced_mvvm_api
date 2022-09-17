646b98302115bcd725ec86df942099d19d3fa63f
Naser Sami <ponanzar@gmail.com>
2022-09-17 18:22:58 +0300

    Adding Streams Implementations in Register ViewModel

--- lib/presentation/register/viewmodel/register_viewmodel.dart
+++ lib/presentation/register/viewmodel/register_viewmodel.dart
@@ -2,15 +2,21 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:advanced_flutter/app/functions.dart';
+import 'package:advanced_flutter/domain/usecase/register_usecase.dart';
 import 'package:advanced_flutter/presentation/base/base_viewmodel.dart';
+import 'package:advanced_flutter/presentation/resources/all_resources.dart';
 
-class RegisterViewModel extends BaseViewModel {
+class RegisterViewModel extends BaseViewModel with RegisterViewModelInput, RegisterViewModelOutput{
   final StreamController _usernameStreamController = StreamController<String>.broadcast();
   final StreamController _mobileNumberStreamController = StreamController<String>.broadcast();
   final StreamController _emailStreamController = StreamController<String>.broadcast();
   final StreamController _passwordStreamController = StreamController<String>.broadcast();
   final StreamController _profilePictureStreamController = StreamController<File>.broadcast();
   final StreamController _areAllInputValidStreamController = StreamController<File>.broadcast();
+
+  final RegisterUseCase _registerUseCase;
+  RegisterViewModel(this._registerUseCase);
 
   @override
   void start() {
@@ -27,6 +33,77 @@ class RegisterViewModel extends BaseViewModel {
     _areAllInputValidStreamController.close();
     super.dispose();
   }
+
+  // ******************************************
+  //? INPUTS
+  // ******************************************
+
+  @override
+  Sink get inputEmail => _emailStreamController.sink;
+
+  @override
+  Sink get inputMobileNumber => _mobileNumberStreamController.sink;
+
+  @override
+  Sink get inputPassword => _passwordStreamController.sink;
+
+  @override
+  Sink get inputProfilePicture => _profilePictureStreamController.sink;
+
+  @override
+  Sink get inputUserName => _usernameStreamController.sink;
+
+
+
+  // ******************************************
+  //? OUTPUTS
+  // ******************************************
+
+  // -------------------------------------------------------------------------
+  // ERRORS
+  @override
+  Stream<String?> get outputErrorEmailValid => outputErrorEmailValid.map((isEmailValid) => isEmailValid != null ? null : StringManager.invalidEmail);
+
+  @override
+  Stream<String?> get outputErrorMobileNumberValid => outputIsMobileNumberValid.map((mobileNumber) => mobileNumber ? null : StringManager.invalidMobileNumber);
+
+  @override
+  Stream<String?> get outputErrorPasswordValid => outputIsPasswordValid.map((password) => password ? null : StringManager.invalidPassword);
+
+  @override
+  Stream<String?> get outputErrorUserNameValid => outputIsUserNameValid.map((isUsernameValid) => isUsernameValid ? null : StringManager.invalidUsername);
+
+  // -------------------------------------------------------------------------
+  // OUTPUTS VALUES
+  @override
+  Stream<bool> get outputIsEmailValid => _emailStreamController.stream.map((event) => isEmailValid(event));
+
+  @override
+  Stream<bool> get outputIsMobileNumberValid => _mobileNumberStreamController.stream.map((event) => _isMobileNumberValid(event));
+
+  @override
+  Stream<bool> get outputIsPasswordValid => _passwordStreamController.stream.map((event) => _isPasswordValid(event));
+
+  @override
+  // TODO: implement outputIsProfilePictureValid
+  Stream<File> get outputIsProfilePictureValid => _profilePictureStreamController.stream.map((file) => file);
+
+  @override
+  Stream<bool> get outputIsUserNameValid => _usernameStreamController.stream.map((event) => _isUserNameValid(event));
+
+  // ******************************************
+  //? PRIVATE FUNCTION
+  // ******************************************
+  bool _isUserNameValid(String username) {
+    return username.length >= 8;
+  }
+  bool _isMobileNumberValid(String mobileNumber) {
+    return mobileNumber.length >= 10;
+  }
+  bool _isPasswordValid(String password) {
+    return password.length >= 8;
+  }
+
 }
 
 abstract class RegisterViewModelInput
@@ -44,12 +121,12 @@ abstract class RegisterViewModelOutput
   Stream<bool> get outputIsMobileNumberValid;
   Stream<bool> get outputIsEmailValid;
   Stream<bool> get outputIsPasswordValid;
-  Stream<bool> get outputIsProfilePictureValid;
+  
+  Stream<File> get outputIsProfilePictureValid;
 
   // another idea ( same result in login but in another way )
-  Stream<String> get outputErrorUserNameValid;
-  Stream<String> get outputErrorMobileNumberValid;
-  Stream<String> get outputErrorEmailValid;
-  Stream<String> get outputErrorPasswordValid;
-  Stream<String> get outputErrorProfilePictureValid;
+  Stream<String?> get outputErrorUserNameValid;
+  Stream<String?> get outputErrorMobileNumberValid;
+  Stream<String?> get outputErrorEmailValid;
+  Stream<String?> get outputErrorPasswordValid;
 }
--- lib/presentation/resources/string_manager.dart
+++ lib/presentation/resources/string_manager.dart
@@ -1,33 +1,37 @@
 class StringManager {
-  static const unDefinedRoute = "Undefined Page";
-  static const onBoardingTitle1 = "SEE THE BEST COURSE #1";
-  static const onBoardingTitle2 = "SEE THE BEST COURSE #2";
-  static const onBoardingTitle3 = "SEE THE BEST COURSE #3";
-  static const onBoardingTitle4 = "SEE THE BEST COURSE #4";
+  static const String unDefinedRoute = "Undefined Page";
+  static const String onBoardingTitle1 = "SEE THE BEST COURSE #1";
+  static const String onBoardingTitle2 = "SEE THE BEST COURSE #2";
+  static const String onBoardingTitle3 = "SEE THE BEST COURSE #3";
+  static const String onBoardingTitle4 = "SEE THE BEST COURSE #4";
 
-  static const onBoardingSubTitle1 = "Tut app is an awesome flutter application using clean architecture #1";
-  static const onBoardingSubTitle2 = "Tut app is an awesome flutter application using clean architecture #2";
-  static const onBoardingSubTitle3 = "Tut app is an awesome flutter application using clean architecture #3";
-  static const onBoardingSubTitle4 = "Tut app is an awesome flutter application using clean architecture #4";
+  static const String onBoardingSubTitle1 = "Tut app is an awesome flutter application using clean architecture #1";
+  static const String onBoardingSubTitle2 = "Tut app is an awesome flutter application using clean architecture #2";
+  static const String onBoardingSubTitle3 = "Tut app is an awesome flutter application using clean architecture #3";
+  static const String onBoardingSubTitle4 = "Tut app is an awesome flutter application using clean architecture #4";
 
   // app strings
-  static const skip = "Skip";
-  static const username = "Username";
-  static const password = "Password";
-  static const login = "Login";
-  static const forgotPassword = "Forgot Password";
-  static const registerText = "Not a member? Sign up";
+  static const String skip = "Skip";
+  static const String username = "Username";
+  static const String password = "Password";
+  static const String login = "Login";
+  static const String forgotPassword = "Forgot Password";
+  static const String registerText = "Not a member? Sign up";
 
   // error strings
-  static const usernameError = "Please enter your username";
-  static const passwordError = "Please enter your password";
+  static const String usernameError = "Please enter your username";
+  static const String passwordError = "Please enter your password";
+  // invalid error strings
+  static const String invalidEmail = "Invalid Email";
+  static const String invalidUsername = "Username must be at least 8 characters";
+  static const String invalidPassword = "password must be at least 8 characters";
+  static const String invalidMobileNumber = "Mobile number must be at least 10 numbers";
 
   // state renderer
-  static const loading = "Loading...";
-  static const retryAgain = "Retry again";
-  static const success = "Success";
-  static const ok = "Ok";
+  static const String loading = "Loading...";
+  static const String retryAgain = "Retry again";
+  static const String success = "Success";
+  static const String ok = "Ok";
   static const String emailHint = 'Email';
-  static const String invalidEmail = "Invalid Email";
   static const String resetPassword = "Reset Password";
 }
